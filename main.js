62c73b0338e5251bf17a6394957c3f42d58aa816
Ryan <kryan@genesco.com>
2020-11-06 06:02:49 -0600

    initialCommit

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("kwrr1");
+
+const fetchUers = async () =>
+  // await( await fetch('http://localhost:9000/getusers')).json();
+  await( await fetch('/.netlify/function/getusers')).json();
+
+  fetchUers().then(data => {
+    userList = document.querySelector('#users');
+
+    data.forEach(user => {
+      const li = document.createElement('li');
+      li.className = 'list-group-item';
+      const link = document.createElement('a');
+      link.appendChild(document.createTextNode(user.login))
+      link.href = user.html_url
+      link.target = '_blank'
+      li.appendChild(link)
+      userList.appendChild(li)
+    })
+  })
